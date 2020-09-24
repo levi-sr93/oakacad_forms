7180685c002b53262f68daaa5a65c6f86cbfdb92
Levi <levi-sr@outlook.com>
2020-09-24 20:37:26 -0300

    Initial Structure, Handling Forms

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SearchInput from './SearchInput'
+
+const App = () => {
+    return (
+        <SearchInput /> 
+    )
+}
+
+export default App;
---
+++ src/components/SearchInput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SearchInput extends React.Component {
+    render(){
+        return(
+            <div>
+                <form>
+                    <input type="text" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchInput;
